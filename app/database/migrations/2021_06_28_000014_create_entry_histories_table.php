2452467c84dc8ac42476dc6659d56419897ebb78
naoya ikeda <ikd708@gmail.com>
2021-06-28 17:08:39 +0900

    add: EntryHistory model

---
+++ app/database/migrations/2021_06_28_000014_create_entry_histories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntryHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entry_histories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('entry_id')->unsigned()->comment('受付ID');
+            $table->foreign('entry_id')->references('id')->on('entries');
+            $table->enum('type', ['Entry', 'Out', 'In', 'Exit'])->comment('履歴タイプ(入場, 一時退室, 再入場, 退場)');
+            $table->timestamp('occurred_at')->comment('発生日時');
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('created_by')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+            $table->timestamp('updated_by')->useCurrent()->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamp('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entry_histories');
+    }
+}
