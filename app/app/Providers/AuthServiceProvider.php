c2453b5cb87fcbbcef15bc46a68c303d138fac79
naoya ikeda <ikd708@gmail.com>
2021-07-16 11:44:46 +0900

    add: admin gate

--- app/app/Providers/AuthServiceProvider.php
+++ app/app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Enums\StaffRole;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +26,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // 管理者権限
+        Gate::define('admin', function ($user) {
+            return ($user->role == StaffRole::ADMIN);
+        });
     }
 }
