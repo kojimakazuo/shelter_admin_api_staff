00fe64b2b3d9d225f80395ad9f65c83608c6e75c
naoya ikeda <ikd708@gmail.com>
2021-08-24 17:21:19 +0900

    add: Gender

---
+++ app/app/Enums/Gender.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+use MyCLabs\Enum\Enum;
+
+final class Gender extends Enum
+{
+    /** 男性 */
+    const MALE = 'Male';
+    /** 女性 */
+    const FEMALE = 'Female';
+}
