2ccc6d180194ccee98e5bb2d1d59b6dfac4722f1
naoya ikeda <ikd708@gmail.com>
2022-03-18 17:25:44 +0900

    add: Deployer #98

---
+++ app/deploy.php
@@ -0,0 +1,52 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'shelter-admin-api');
+
+// Project repository
+set('repository', 'git@shelter-admin-api.github.com:sharecrest/shelter-admin-api.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', false);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', ['vendor']);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+inventory('hosts.yml');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+before('deploy:symlink', 'artisan:migrate');
+
+// Change Release Path
+after('deploy:update_code', 'change_dir');
+
+task('change_dir', function () {
+    set('release_path', get('release_path') . DIRECTORY_SEPARATOR . 'app');
+    run('cd {{release_path}}');
+});
+
+// Copy .env file
+before('deploy:shared','deploy:copy:env');
+
+task('deploy:copy:env', function () {
+    run("cp {{release_path}}/.env.{{stage}} {{deploy_path}}/shared/.env");
+});
+
+// php-fpm restart
+after('success', 'php-fpm:restart');
+
+task('php-fpm:restart', function() {
+    run('sudo service php-fpm restart');
+});
